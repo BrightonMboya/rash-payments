388065b6ef61647d0035f7169ef209c01565077d
brightonmboya <b.mboya@alustudent.com>
2024-09-03 18:55:15 +0300

    added the redirects

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,26 +1,4 @@
-// import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
-// import styles from "./index.module.css";
-// import Services from "../components/landingpage/Services";
-// import Hero from "../components/landingpage/Hero";
-// import Proposal from "../components/landingpage/Proposal";
-// import Cases from "../components/landingpage/Cases";
-// import Contact from "../components/landingpage/Contact";
-
-// export default function Home(): JSX.Element {
-//   const { siteConfig } = useDocusaurusContext();
-//   return (
-//     // <Layout title="">
-//     <>
-//       <Hero />
-//       <Services />
-//       <Proposal />
-//       <Cases />
-//       <Contact />
-//     </>
-
-//     // </Layout>
-//   );
-// }
+import { Redirect } from "@docusaurus/router";
 import clsx from "clsx";
 import Link from "@docusaurus/Link";
 import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
@@ -30,37 +8,7 @@ import Heading from "@theme/Heading";
 
 import styles from "./index.module.css";
 
-function HomepageHeader() {
-  const { siteConfig } = useDocusaurusContext();
-  return (
-    <header className={clsx("hero hero--primary", styles.heroBanner)}>
-      <div className="container">
-        <Heading as="h1" className="hero__title">
-          {siteConfig.title}
-        </Heading>
-        <p className="hero__subtitle">{siteConfig.tagline}</p>
-        <div className={styles.buttons}>
-          <Link
-            className="button button--secondary button--lg"
-            to="/docs/intro">
-            Docusaurus Tutorial - 5min ⏱️
-          </Link>
-        </div>
-      </div>
-    </header>
-  );
-}
-
 export default function Home(): JSX.Element {
-  const { siteConfig } = useDocusaurusContext();
-  return (
-    <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
-      <HomepageHeader />
-      <main>
-        <HomepageFeatures />
-      </main>
-    </Layout>
-  );
+
+  return <Redirect to="/docs/api/products/product_object" />;
 }
