d187febb1719db0b17c76373a72a1bf489611488
brightonmboya <b.mboya@alustudent.com>
2024-09-03 18:56:39 +0300

    removed unwanted files

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,4 @@
 import { Redirect } from "@docusaurus/router";
-import clsx from "clsx";
-import Link from "@docusaurus/Link";
-import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
-import Layout from "@theme/Layout";
-import HomepageFeatures from "@site/src/components/HomepageFeatures";
-import Heading from "@theme/Heading";
-
-import styles from "./index.module.css";
 
 export default function Home(): JSX.Element {
 
